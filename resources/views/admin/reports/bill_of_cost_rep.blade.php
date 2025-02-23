d669f951ecbbcd76163d6e0608d724d314af2b8f
ikram50 <muhammadikrami749@gmail.com>
2025-02-23 23:24:40 +0500

    Ikram

--- resources/views/admin/reports/bill_of_cost_rep.blade.php
+++ resources/views/admin/reports/bill_of_cost_rep.blade.php
@@ -1,5 +1,12 @@
 @extends('admin.layout.app')
-
+<style>
+    .desc_width{
+        width: 22% !important;
+    }
+    .ledger_dorpdown{
+        width: 31% !important;
+    }
+</style>
 @section('content')
     <div class="main-content app-content">
         <div class="container-fluid">
@@ -11,25 +18,23 @@
                             <h4 class="card-title">Bill Of Cost Report</h4>
                         </div>
                         <div class="card-body">
-                            <div class=" col-lg-12 ">
-
-                                <!-- Filter Form -->
-                                <form method="GET" id="filter-form">
+                            <div class="col-lg-12">
+                                <form method="GET" id="filter-form1">
                                     <div class="row mb-4">
+                                        <!-- Left Section: Ledger Ref and View Report Button -->
                                         <div class="col-md-4">
                                             <label for="ledger_ref">Ledger Ref:</label>
-                                            <input type="text" id="ledger_ref" name="ledger_ref" class="form-control  "
-                                                placeholder="">
+                                            <input type="text" id="ledger_ref" name="ledger_ref" class="form-control" placeholder="Ledger Ref">
+                                            <input type="hidden" id="File_id" name="File_id" class="form-control">
+                                            <div id="results-dropdown" class="dropdown-menu ledger_dorpdown" aria-labelledby="ledger_ref"></div>
                                         </div>
-
+                            
                                         <div class="col-md-4 d-flex align-items-end">
-                                            <div class="ms-2">
-                                                <button type="button" id="filter-btn" class="btn btn-primary">View
-                                                    Report</button>
-                                            </div>
+                                            <button type="button" id="filter-btn" class="btn btn-primary">View Report</button>
                                         </div>
-                                        <div class="col-md-4  d-flex justify-content-end align-items-center  mt-3">
-
+                            
+                                        <!-- Right Section: Download Buttons -->
+                                        <div style="display: none !important" class="col-md-4 d-flex justify-content-end align-items-end doc_buttons">
                                             <button id="download-pdf" class="btn btn-danger me-2">Download PDF</button>
                                             <button id="download-csv" class="btn btn-success">Download CSV</button>
                                         </div>
@@ -38,34 +43,34 @@
                             </div>
 
                             <!-- Table Section (Initially Hidden) -->
-                            <div id="table-section" class="mt-4">
+                            <div id="table-section" class="mt-4" style="display: none" >
                                 <div class="table-responsive">
 
                                     <table class="table table-bordered table-striped">
                                         <tbody>
                                             <tr>
                                                 <td>Client Name:</td>
-                                                <td colspan="5">Tawqir Asghar</td>
+                                                <td class="client_name" colspan="5"></td>
                                             </tr>
                                             <tr>
                                                 <td>Client Address:</td>
-                                                <td colspan="5">123 shaggy calf lane </td>
+                                                <td class="client_addres" colspan="5"></td>
                                             </tr>
                                             <tr>
                                                 <td>Matter</td>
-                                                <td>IMM</td>
+                                                <td class="matter_name"></td>
                                                 <td>Bill Date:</td>
-                                                <td>15-02-2025</td>
+                                                <td class="bill_date"></td>
                                                 <td colspan="2"></td>
 
                                             </tr>
                                             <tr>
                                                 <td>Ledger Ref</td>
-                                                <td><a class="ledger-link"
+                                                <td class="ledger_refs"><a class="ledger-link"
                                                         onclick="javscript:openClientLedger(6500,'666666');"
-                                                        href="javascript:void(0);">666666</a></td>
+                                                        href="javascript:void(0);"></a></td>
                                                 <td>Bill Ref:</td>
-                                                <td>645,test</td>
+                                                <td class="bill_ref" ></td>
                                                 <td colspan="2"></td>
                                             </tr>
                                             <tr>
@@ -90,12 +95,12 @@
                                                 <td align="center"><strong></strong></td>
                                             </tr>
                                             <tr>
-                                                <td colspan="2"><a class="ledger-link"
+                                                <td class="description" colspan="2"><a class="ledger-link"
                                                         onclick="javascript: EditProject(196688);"
-                                                        href="javascript:void(0);">4</a></td>
-                                                <td align="center">45.00</td>
-                                                <td align="center">0.00</td>
-                                                <td align="center">45.00</td>
+                                                        href="javascript:void(0);"></a></td>
+                                                <td class="net_amout" align="center"> </td>
+                                                <td class="vat" align="center"> </td>
+                                                <td class="total" align="center"> </td>
                                                 <td></td>
                                             </tr>
                                            
@@ -103,7 +108,7 @@
                                                 <td align="right"><strong>Our Costs total</strong></td>
                                                 <td colspan="4">&nbsp;</td>
 
-                                                <td align="right">1,752.00</td>
+                                                <td class="cost_total" align="right">1,752.00</td>
                                             </tr>
                                             <tr>
                                                 <td colspan="6"><strong>Disbursments</strong></td>
@@ -113,7 +118,7 @@
                                                 <td colspan="1" align="right"><strong>Disbursments Total</strong></td>
                                                 <td colspan="4">&nbsp;</td>
 
-                                                <td align="right" style="border:1px">0.00</td>
+                                                <td class="disbursments_tota;" align="right" style="border:1px"></td>
                                             </tr>
                                             <tr>
                                                 <td colspan="6">&nbsp;</td>
@@ -123,7 +128,7 @@
 
                                                 <td align="right"><strong>Bill Total</strong></td>
 
-                                                <td align="right">1,752.00</td>
+                                                <td class="bill_total" align="right"></td>
                                             </tr>
                                             <tr>
                                                 <td colspan="6">&nbsp;</td>
@@ -133,7 +138,7 @@
 
                                                 <td align="right"><strong>Payment Received</strong></td>
 
-                                                <td align="right">1,752.00</td>
+                                                <td class="payment_received" align="right"></td>
                                             </tr>
                                             <tr>
                                                 <td colspan="6">&nbsp;</td>
@@ -143,7 +148,7 @@
 
                                                 <td align="right"><strong>Outstanding Balance</strong></td>
 
-                                                <td align="right">0.00</td>
+                                                <td class="outstanding_balance" align="right"></td>
                                             </tr>
                                             <tr>
                                                 <td colspan="6">&nbsp;</td>
@@ -181,9 +186,7 @@
                                     </table>
 
 
-                                    {{-- <div id="pagination-links" class="mt-3">
-                                        <!-- Pagination links will be appended here -->
-                                    </div> --}}
+                                  
                                 </div>
                             </div>
 
@@ -197,101 +200,90 @@
 
 @push('scripts')
     <script>
-        $(document).ready(function () {
+        document.getElementById('ledger_ref').addEventListener('input', function() {
+    const query = this.value;
 
+    if (query.length < 2) {
+        document.getElementById('results-dropdown').innerHTML = '';
+        document.getElementById('results-dropdown').classList.remove('show');
+        return;
+    }
 
-            $('#filter-btn').click(function () {
-                var fromDate = $('#from_date').val();
-                var toDate = $('#to_date').val();
+    fetch(`/search-ledger?query=${query}`)
+        .then(response => response.json())
+        .then(data => {
+            const dropdown = document.getElementById('results-dropdown');
+            dropdown.innerHTML = '';
 
-                if (fromDate && toDate) {
-                    $.ajax({
-                        url: "{{ route('file.report.data') }}",
-                        type: "GET",
-                        data: { from_date: fromDate, to_date: toDate },
-                        success: function (response) {
-                            $('#table-body').empty();
-                            $('#pagination-links').empty();
-
-                            if (response.data.length > 0) {
-                                $.each(response.data, function (index, record) {
-                                    let status_l = "";
-
-                                    switch (record.Status) {
-                                        case 'L':
-                                            status_l = "Live";
-                                            break;
-                                        case 'C':
-                                            status_l = "Close";
-                                            break;
-                                        case 'A':
-                                            status_l = "Abortive";
-                                            break;
-                                        case 'I':
-                                            status_l = "Close Abortive";
-                                            break;
-                                        default:
-                                            status_l = "Unknown";
-                                            break;
-                                    }
-
-                                    $('#table-body').append(`
-                                        <tr>
-                                            <td>${index + 1}</td>
-                                            <td>${(formatDates(record.File_Date))}</td>
-                                            <td>
-                                                <a class='text-primary' href="/file/update/${record.File_ID || ''}">
-                                                    ${record.Ledger_Ref || 'N/A'}
-                                                </a>
-                                            </td>
-
-                                            <td>${record.Matter}</td>
-                                            <td>${record.First_Name} ${record.Last_Name}</td>
-                                            <td>${record.Address1} ${record.Address2} ${record.Town} ${record.Post_Code}</td>
-                                            <td>${record.Fee_Earner}</td>
-                                            <td>${status_l}</td>
-                                            <td>${record.File_Date}</td>
-                                        </tr>
-                                    `);
-                                });
-
-                                $('#pagination-links').html(response.pagination);
-                                $('#table-section').show();
-                                $('#display-from-date').text(formatDate($('#from_date').val()));
-                                $('#display-to-date').text(formatDate($('#to_date').val()));
-
-                                function formatDate(date) {
-                                    if (!date) return ''; // Handle empty date
-                                    let parts = date.split('-'); // Split YYYY-MM-DD
-                                    return `${parts[2]}/${parts[1]}/${parts[0]}`; // Convert to DD/MM/YYYY
-                                }
-                                function formatDates(dateString) {
-                                    if (!dateString) return 'N/A';
-
-                                    let dateObj = new Date(dateString);
-                                    let day = String(dateObj.getDate()).padStart(2, '0');
-                                    let month = String(dateObj.getMonth() + 1).padStart(2, '0');
-                                    let year = dateObj.getFullYear();
-
-                                    return `${day}/${month}/${year}`;
-                                }
-
-                            } else {
-                                $('#table-body').html('<tr><td colspan="11" class="text-center">No records found</td></tr>');
-                                $('#table-section').show();
-                            }
-                        },
-                        error: function () {
-                            alert('Something went wrong. Please try again.');
-                        }
+            if (data.length > 0) {
+                data.forEach(item => {
+                    const option = document.createElement('a');
+                    option.classList.add('dropdown-item');
+                    option.href = '#';
+                    option.textContent = item.Ledger_Ref;  
+                    option.addEventListener('click', function() {
+                        document.getElementById('ledger_ref').value = item.Ledger_Ref;
+                        dropdown.classList.remove('show');
                     });
-                } else {
-                    alert('Please select both From Date and To Date.');
+                    option.addEventListener('click', function () {
+                    document.getElementById('ledger_ref').value = item.Ledger_Ref;
+
+                    document.getElementById('File_id').value = item.file_id;
+
+                    dropdown.classList.remove('show');
+                });
+                    dropdown.appendChild(option);
+                });
+                dropdown.classList.add('show');
+            } else {
+                dropdown.classList.remove('show');
+            }
+        })
+        .catch(error => console.error('Error:', error));
+});
+$(document).ready(function () {
+    $('#filter-btn').click(function () {
+        var File_id = $('#File_id').val();
+        var ledger_ref = $('#ledger_ref').val();
+
+        if (File_id && ledger_ref) {
+            $.ajax({
+                url: "{{ route('bill.of.cost.data') }}",
+                type: "GET",
+                data: { File_id: File_id, ledger_ref: ledger_ref },
+                success: function (response) {
+                    $('#table-body').empty(); // Clear table
+                    $('#Client_Ref').text(response.Client_Ref);
+                     
+                },
+                error: function (xhr, status, error) {
+                    console.error('Error:', error);
+                    alert('Something went wrong. Please try again.');
                 }
             });
-        });
-
-      
+        } else {
+            alert('Please ensure both File ID and Ledger Ref are selected.');
+        }
+    });
+});
+     console.error('Error:', error);
+                    alert('Something went wrong. Please try again.');
+                }
+            });
+        } else {
+            alert('Please ensure both File ID and Ledger Ref are selected.');
+        }
+    });
+});
+     console.error('Error:', error);
+                    alert('Something went wrong. Please try again.');
+                }
+            });
+        } else {
+            alert('Please ensure both File ID and Ledger Ref are selected.');
+        }
+    });
+});
  
         $(document).ready(function () {
             $('#download-pdf').click(function () {
